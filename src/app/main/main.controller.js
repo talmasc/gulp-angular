dcf44647b5c8410f94953e9d57a744a8619e3408
talmasc <eric.l.hillenbrand@gmail.com>
2016-08-23 14:04:27 -0700

    uypdated

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,6 +2,14 @@ export class MainController {
   constructor ($http) {
     'ngInject';
     this.$http = $http;
+    this.getMessages();
+  }
+
+  getMessages() {
+    var vm = this;
+    this.$http.get('http://localhost:5000/api/message').then(function(result) {
+      vm.messages = result.data;
+    });
   }
 
   postMessage() {
